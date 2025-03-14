862f3a4929b6395bd7c57142359127bba39e6d3d
marcos.henrique <wakeupmh@gmail.com>
2025-03-14 13:02:55 -0300

    fix: using VITE prefix in url

--- src/pages/UploadForm.tsx
+++ src/pages/UploadForm.tsx
@@ -50,7 +50,7 @@ export const UploadForm = () => {
     formData.append('file', file);
 
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/upload`, {
+      const response = await fetch(`${process.env.VITE_API_URL}/upload`, {
         method: 'POST',
         body: formData,
       });
