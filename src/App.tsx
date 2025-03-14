71322d880c7b4fe88f9365ead700145b774b528e
marcos.henrique <wakeupmh@gmail.com>
2025-03-14 12:13:37 -0300

    fix: remove unecessary React import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Contacts } from './pages/Contacts';
 import { UploadForm } from './pages/UploadForm';
